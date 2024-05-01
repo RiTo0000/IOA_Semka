d5a21772e0700ca13e813a2dc6683e445220cd7d
RiTo0000 <osir.samot@gmail.com>
2024-05-01 17:16:28 +0200

    initial

---
+++ src/main/java/com/mycompany/semestralka/Semestralka.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.semestralka;
+
+/**
+ *
+ * @author namer
+ */
+public class Semestralka {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
