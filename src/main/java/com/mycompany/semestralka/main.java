9f950bbd783f829cd77c6492ee1916d5dbd16a69
RiTo0000 <osir.samot@gmail.com>
2024-05-01 17:20:57 +0200

    initial

--- src/main/java/com/mycompany/semestralka/main.java
+++ src/main/java/com/mycompany/semestralka/main.java
@@ -1,15 +1,14 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.semestralka;
 
 /**
  *
  * @author namer
  */
-public class Semestralka {
-
+public class main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
